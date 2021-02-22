e57c8039a7e88b83a9f057907d244649b23f7f2b
Robyn Tyler <robynstyler@outlook.com>
2021-02-22 12:51:14 +0000

    Added iGenerate to Chummer and Flicker.

--- server/src/main/java/com/example/codeclan/abductionationservice/models/generators/Chummer.java
+++ server/src/main/java/com/example/codeclan/abductionationservice/models/generators/Chummer.java
@@ -1,9 +1,16 @@
 package com.example.codeclan.abductionationservice.models.generators;
 
-public class Chummer extends Generator {
+import com.example.codeclan.abductionationservice.models.Abductee;
+
+public class Chummer extends Generator implements iGenerate {
 
     public Chummer(){
         super("Chummer", "Food");
     }
 
+    @Override
+    public double generate(Abductee abductee) {
+        double result = (abductee.getWeight() * abductee.getDensity()) / 10 ;
+        return result;
+    }
 }
--- server/src/main/java/com/example/codeclan/abductionationservice/models/generators/Flicker.java
+++ server/src/main/java/com/example/codeclan/abductionationservice/models/generators/Flicker.java
@@ -1,9 +1,16 @@
 package com.example.codeclan.abductionationservice.models.generators;
 
-public class Flicker extends Generator {
+import com.example.codeclan.abductionationservice.models.Abductee;
+
+public class Flicker extends Generator implements iGenerate {
 
     public Flicker(){
         super("Flicker", "Fuel");
     }
 
+    @Override
+    public double generate(Abductee abductee) {
+        double result = (abductee.getWeight() * abductee.getDensity()) / 50 ;
+        return result;
+    }
 }
