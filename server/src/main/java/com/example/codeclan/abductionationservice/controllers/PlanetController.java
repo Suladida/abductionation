c20bd4cb45de3ccc459e7404885ad40a4357efab
Robyn Tyler <robynstyler@outlook.com>
2021-02-22 13:25:40 +0000

    Added Planet controller.

---
+++ server/src/main/java/com/example/codeclan/abductionationservice/controllers/PlanetController.java
@@ -0,0 +1,37 @@
+package com.example.codeclan.abductionationservice.controllers;
+
+import com.example.codeclan.abductionationservice.models.Abductee;
+import com.example.codeclan.abductionationservice.models.Planet;
+import com.example.codeclan.abductionationservice.repositories.AbducteeRepository;
+import com.example.codeclan.abductionationservice.repositories.PlanetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PlanetController {
+
+    @Autowired
+    PlanetRepository planetRepository;
+
+    @GetMapping(value="/planets")
+    public ResponseEntity<List<Planet>> getAllPlanets(){
+        return new ResponseEntity<>(planetRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/planets/{id}")
+    public ResponseEntity<Optional<Planet>> getPlanet(@PathVariable Long id){
+        return new ResponseEntity<>(planetRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/planets")
+    public ResponseEntity<Planet> createPlanet(@RequestBody Planet planet) {
+        planetRepository.save(planet);
+        return new ResponseEntity<>(planet, HttpStatus.CREATED);
+    }
+
+}
